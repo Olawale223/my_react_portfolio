171f65b3557eabd0e96c5e23ce3d17ff8f8db9b3
ola wale <90061003+Olawale223@users.noreply.github.com>
2026-04-08 21:09:32 +0100

    feat: add Footer component with contact information and social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,62 +3,62 @@ import React from 'react'
 function Footer() {
   return (
     <footer className="footer">
-    <div className="container">
+      <div className="container">
 
-      <ul className="footer-list">
+        <ul className="footer-list">
 
-        <li>
-          <p className="h4 footer-list-title">Get in touch</p>
-        </li>
+          <li>
+            <p className="h4 footer-list-title">Get in touch</p>
+          </li>
 
-        <li>
-          <a href="mailto:lawalabdullateef16@gmail.com" className="footer-link">lawalabdullateef16@gmail.com</a>
-        </li>
+          <li>
+            <a href="mailto:lawalabdullateef16@gmail.com" className="footer-link">lawalabdullateef16@gmail.com</a>
+          </li>
 
-      </ul>
+        </ul>
 
-      <ul className="footer-list">
+        <ul className="footer-list">
 
-        <li>
-          <p className="h4 footer-list-title">Locations</p>
-        </li>
+          <li>
+            <p className="h4 footer-list-title">Locations</p>
+          </li>
 
-        <li>
-          <a href="#" className="footer-link">Lagos - Nigeria</a>
-        </li>
+          <li>
+            <a href="#" className="footer-link">Lagos - Nigeria</a>
+          </li>
 
-      </ul>
+        </ul>
 
-      <ul className="social-list">
+        <ul className="social-list">
 
-        <li>
-          <a href="https://github.com/Olawale223" className="social-link">
-            <ion-icon name="logo-github"></ion-icon>
-          </a>
-        </li>
+          <li>
+            <a href="https://github.com/Olawale223" className="social-link">
+              <ion-icon name="logo-github"></ion-icon>
+            </a>
+          </li>
 
-        <li>
-          <a href="#" className="social-link">
-            <ion-icon name="logo-instagram"></ion-icon>
-          </a>
-        </li>
+          <li>
+            <a href="#" className="social-link">
+              <ion-icon name="logo-instagram"></ion-icon>
+            </a>
+          </li>
 
-        <li>
-          <a href="#" className="social-link">
-            <ion-icon name="logo-skype"></ion-icon>
-          </a>
-        </li>
+          <li>
+            <a href="#" className="social-link">
+              <ion-icon name="logo-skype"></ion-icon>
+            </a>
+          </li>
 
-        <li>
-          <a href="#" className="social-link">
-            <ion-icon name="logo-linkedin"></ion-icon>
-          </a>
-        </li>
+          <li>
+            <a href="https://www.linkedin.com/in/abdullateef-lawal-28503429b/" className="social-link">
+              <ion-icon name="logo-linkedin"></ion-icon>
+            </a>
+          </li>
 
-      </ul>
+        </ul>
 
-    </div>
-  </footer>
+      </div>
+    </footer>
   )
 }
 
